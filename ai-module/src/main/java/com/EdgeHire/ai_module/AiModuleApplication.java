34a705615a05587399967bb4b19d3ee09e190692
RaphX <14630825+raphx@user.noreply.gitee.com>
2025-06-24 09:08:06 +0800

    Initialize repo

---
+++ ai-module/src/main/java/com/EdgeHire/ai_module/AiModuleApplication.java
@@ -0,0 +1,13 @@
+package com.EdgeHire.ai_module;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AiModuleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AiModuleApplication.class, args);
+	}
+
+}
---
+++ backEnd/src/main/java/com/se/EdgeHire/EdgeHireApplication.java
@@ -0,0 +1,13 @@
+package com.se.EdgeHire;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EdgeHireApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EdgeHireApplication.class, args);
+	}
+
+}
