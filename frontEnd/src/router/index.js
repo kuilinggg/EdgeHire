1a962af5e85d4ed2526c30e9480273e2cf8f37cd
xiong <14635444+xiong_yc@user.noreply.gitee.com>
2025-06-26 02:09:16 +0000

    update frontEnd/src/router/index.js.
    Signed-off-by: xiong <14635444+xiong_yc@user.noreply.gitee.com>

--- frontEnd/src/router/index.js
+++ frontEnd/src/router/index.js
@@ -2,7 +2,7 @@ import { createRouter, createWebHistory } from 'vue-router'
 
 const routes = [
   {
-    path: '/login',
+    path: '/',
     name: 'Login',
     component: () => import('../views/Login/Login.vue'),
   },
@@ -54,7 +54,7 @@ const routes = [
     component: () => import('../views/Admin/AdminLayout.vue'),
     children: [
       {
-        path:'/',
+        path:'',
         redirect:'/users'
       },
       {
