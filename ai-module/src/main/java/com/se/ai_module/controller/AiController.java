a836740ce5e3fd93ebd2d6351e5f0dd98a0f5a50
RaphX <14630825+raphx@user.noreply.gitee.com>
2025-06-26 09:58:34 +0800

    初步实现AI微服务与后端接口连接

--- ai-module/src/main/java/com/se/ai_module/controller/AiController.java
+++ ai-module/src/main/java/com/se/ai_module/controller/AiController.java
@@ -7,10 +7,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.ai.chat.client.ChatClient;
 import org.springframework.ai.chat.memory.ChatMemory;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.reactive.function.client.WebClientResponseException;
 import reactor.core.publisher.Flux;
 
@@ -53,7 +50,7 @@ public class AiController {
                 });
     }
 
-    @GetMapping(value = "/optimize")
+    @PostMapping(value = "/optimize")
     public String optimize(Map<String, String> request) {
         String conversationId = request.get("id");
         String resumeContent = request.get("resume");
@@ -64,8 +61,8 @@ public class AiController {
                 .content();
     }
 
-    @GetMapping(value = "/optimizeStream")
-    public Flux<String> optimizeStream(Map<String, String> request) {
+    @PostMapping(value = "/optimizeStream")
+    public Flux<String> optimizeStream(@RequestBody Map<String, String> request) {
         String conversationId = request.get("id");
         String resumeContent = request.get("resume");
         return chatClient
---
+++ backEnd/src/main/java/com/se/EdgeHire/Config/WebClientConfiguration.java
@@ -0,0 +1,16 @@
+package com.se.EdgeHire.Config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.reactive.function.client.WebClient;
+
+@Configuration
+public class WebClientConfiguration {
+    @Bean
+    public WebClient webClient() {
+        return WebClient.builder()
+                .baseUrl("http://127.0.0.1:8081")
+                .defaultHeader("Content-Type", "application/json")
+                .build();
+    }
+}
