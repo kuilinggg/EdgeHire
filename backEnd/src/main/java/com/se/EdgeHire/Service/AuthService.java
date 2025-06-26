bd587024887b94c8e519a7aec38755ae9ffb9a66
Firefly-26710 <2235501805@qq.com>
2025-06-26 14:58:51 +0800

    密码加密

--- backEnd/src/main/java/com/se/EdgeHire/Service/AuthService.java
+++ backEnd/src/main/java/com/se/EdgeHire/Service/AuthService.java
@@ -4,6 +4,7 @@ import com.se.EdgeHire.Entity.User;
 import com.se.EdgeHire.Repository.UserRepository;
 import com.se.EdgeHire.Util.JwtUtil;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.util.Optional;
@@ -14,10 +15,12 @@ public class AuthService {
     @Autowired
     private UserRepository userRepository;
 
+    private final BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder(); // 新增
+
     // 登录
     public Optional<User> login(String username, String password) {
         Optional<User> userOpt = userRepository.findByUsername(username);
-        if (userOpt.isPresent() && userOpt.get().getPassword().equals(password)) {
+        if (userOpt.isPresent() && passwordEncoder.matches(password, userOpt.get().getPassword())) { //密码匹配
             return userOpt;
         }
         return Optional.empty();
@@ -31,6 +34,7 @@ public class AuthService {
         if ((user.getRole() != 0 && user.getRole() != 1 && user.getRole() != 2)) {
             throw new RuntimeException("角色非法");
         }
+        user.setPassword(passwordEncoder.encode(user.getPassword())); // 哈希算法加密
         return userRepository.save(user);
     }
 
