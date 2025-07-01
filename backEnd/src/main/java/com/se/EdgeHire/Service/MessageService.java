9fa84cb1cc534a6efd030b1da705c309fccff0b8
RaphX <14630825+raphx@user.noreply.gitee.com>
2025-07-01 16:37:26 +0800

    优化处理已读的效率

--- backEnd/src/main/java/com/se/EdgeHire/Service/MessageService.java
+++ backEnd/src/main/java/com/se/EdgeHire/Service/MessageService.java
@@ -4,10 +4,8 @@ import com.se.EdgeHire.Entity.Message;
 import com.se.EdgeHire.Repository.MessageRepository;
 import jakarta.transaction.Transactional;
 import lombok.AllArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.util.Comparator;
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
