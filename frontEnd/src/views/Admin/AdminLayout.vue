9c40fa8ca1304f3ff455b800467c87d49f408ebf
RaphX <14630825+raphx@user.noreply.gitee.com>
2025-07-03 16:18:09 +0800

    修改管理员头像

--- frontEnd/src/views/Admin/AdminLayout.vue
+++ frontEnd/src/views/Admin/AdminLayout.vue
@@ -37,8 +37,7 @@
       <header class="header">
         <el-dropdown trigger="hover">
           <span class="user-info">
-            <el-avatar :src="avatar" size="40" icon="el-icon-user" />
-             <img class="avatar" :src="avatarUrl" alt="avatar" />
+            <el-avatar :src="avatarUrl" size="40" icon="el-icon-user" />
           </span>
           <template #dropdown>
             <el-dropdown-menu>
@@ -62,7 +61,7 @@ import { authApi } from '../../api/auth'
 import { ElMessage } from 'element-plus'
 import { User, Briefcase, DataAnalysis, Monitor } from '@element-plus/icons-vue'
 
-const avatarUrl = ref('https://api.dicebear.com/7.x/miniavs/svg?seed=jobseeker')
+const avatarUrl = ref('http://localhost:8080/api/files/admin.png')
 const router=useRouter()
 const avatar = ref(null)
 const logout = () => {
