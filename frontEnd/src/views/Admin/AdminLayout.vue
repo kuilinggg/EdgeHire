5e67568f5eb08975e98d1e56a340e30113fbbd64
RaphX <14630825+raphx@user.noreply.gitee.com>
2025-07-03 16:29:20 +0800

    优化样式

--- frontEnd/src/views/Admin/AdminLayout.vue
+++ frontEnd/src/views/Admin/AdminLayout.vue
@@ -89,7 +89,7 @@ const logout = () => {
 .logo-container {
   display: flex;
   align-items: center;
-  justify-content: space-between;
+  justify-content: center;
   padding: 20px;
   border-bottom: 1px solid #f0f0f0;
   background: #fff;
